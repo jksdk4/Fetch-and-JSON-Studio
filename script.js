a4ecfa42a9978bbff75375dcbbe0cc37a922ced9
Jasmine <jasspears@gmail.com>
2019-12-08 10:03:12 -0600

    completed studio, modified JS and style

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+	let fetchURL = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+	fetchURL.then(function(response){
+		response.json().then(function(json){
+			const containerDiv = document.getElementById("container");
+			for (let i = 0; i < json.length; i++){
+				let color = "initial";
+				if (json[i].active){
+					color = "green";
+				}
+				containerDiv.innerHTML += `
+					<div class="astronaut">
+					   <div class="bio">
+					      <h3>${json[i].firstName} ${json[i].lastName}</h3>
+					      <ul>
+					         <li>Hours in space: ${json[i].hoursInSpace}</li>
+					         <li style=color:${color}>Active: ${json[i].active}</li>
+					         <li>Skills: ${json[i].skills.join(", ")}</li>
+					      </ul>
+					   </div>
+					   <img class="avatar" src="${json[i].picture}">
+					</div>
+				`;
+			}
+		});
+	});
+});
